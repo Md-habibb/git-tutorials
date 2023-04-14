d364ad8769b11d5bfa7de37fa752bb53066906fe
habib <itshabibb0@gmail.com>
2023-04-14 22:20:33 +0600

    created a new addition function in java file!

--- hello.java
+++ hello.java
@@ -1,10 +1,17 @@
 public class Hello {
+    
     public static void main(strind args[]) {
         System.out.println('I love vs code editor so much');
         diplay('habib');
+        addition(10, 33);
     }
 
     public static void diplay(String message) {
         System.out.println('hello ' + message);
     }
+
+    public static void addition(int a, int b) {
+        System.out.println(a + b);
+    }
+
 }
