200734a10a6e3de9104da53a71270649edf1d978
habib <itshabibb0@gmail.com>
2023-04-14 22:00:42 +0600

    Added new display function on java file

--- hello.java
+++ hello.java
@@ -1,5 +1,10 @@
 public class Hello {
     public static void main(strind args[]) {
         System.out.println('I love vs code editor so much');
+        diplay('habib');
+    }
+
+    public static void diplay(String message) {
+        System.out.println('hello ' + message);
     }
 }
